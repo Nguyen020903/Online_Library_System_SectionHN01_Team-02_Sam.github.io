77c2a2ae89fe5ad41220f7c590a8db7e8acf8665
My Le <hmyle.it@gmail.com>
2023-11-28 11:26:31 +0700

    Update NPM Package

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// Middleware
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+// Database Connection
+const dbURI =
+  'mongodb+srv://hmyle:C6lrHMWYYDO2K5Bz@cluster0.sujlcna.mongodb.net/';
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+// Homepage
+app.get('/', async (req, res) => {
+    res.render('login.html');
+});
+
+
+// Port Connect
+app.listen(port, () => {
+    console.log("port connected");
+  });
