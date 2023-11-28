d5f27f79dfd16a132fece4da9f0023b0c9a75267
My Le <hmyle.it@gmail.com>
2023-11-28 11:55:26 +0700

    Update User Schema

---
+++ models/user.js
@@ -0,0 +1,81 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    userType: {
+        type: String,
+        require: true
+    },
+    userFullName: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    admissionId: {
+        type: String,
+        min: 3,
+        max: 15,
+    },
+    employeeId: {
+        type: String,
+        min: 3,
+        max: 15,
+    },
+    age: {
+        type: Number
+    },
+    gender: {
+        type: String
+    },
+    dob: {
+        type: String
+    },
+    address: {
+        type: String,
+        default: ""
+    },
+    mobileNumber: {
+        type: Number,
+        require: true
+    },
+    photo: {
+        type: String,
+        default: ""
+    },
+    email: {
+        type: String,
+        require: true,
+        max: 50,
+        unique: true
+    },
+    password: {
+        type: String,
+        require: true,
+        min: 6
+    },
+    points: {
+        type: Number,
+        default: 0
+    },
+    activeTransactions: [{
+        type: mongoose.Types.ObjectId,
+        ref: "BookTransaction"
+    }],
+    prevTransactions: [{
+        type: mongoose.Types.ObjectId,
+        ref: "BookTransaction"
+    }],
+    isAdmin: {
+        type: Boolean,
+        default: false
+    }
+},
+    {
+        timestamps: true
+});
+
+
+// Define a model based on the schema
+const User = mongoose.model('User', productSchema);
+
+// the module exports the "Product" model so that it can be used by other parts of the application.
+module.exports = User;
