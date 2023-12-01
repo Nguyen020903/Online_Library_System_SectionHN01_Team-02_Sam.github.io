70aa26f28774e8277e3a38ddef43801259ffafbb
Vaeri8 <Vaeri8@users.noreply.github.com>
2023-12-02 01:46:53 +0700

    bookshelf
    need to polish + add swiper

--- script.js
+++ script.js
@@ -26,3 +26,96 @@ function copyToClipboard(elementId) {
     alert('Email address copied to clipboard!');
 }
 
+const track = document.getElementById("image-track");
+
+const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
+
+const handleOnUp = () => {
+  track.dataset.mouseDownAt = "0";  
+  track.dataset.prevPercentage = track.dataset.percentage;
+}
+
+const handleOnMove = e => {
+  if(track.dataset.mouseDownAt === "0") return;
+  
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+        maxDelta = window.innerWidth / 2;
+  
+  const percentage = (mouseDelta / maxDelta) * -100,
+        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
+        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+  
+  track.dataset.percentage = nextPercentage;
+  
+  track.animate({
+    transform: `translate(${nextPercentage}%, -50%)`
+  }, { duration: 1200, fill: "forwards" });
+  
+  for(const image of track.getElementsByClassName("image")) {
+    image.animate({
+      objectPosition: `${100 + nextPercentage}% center`
+    }, { duration: 1200, fill: "forwards" });
+  }
+}
+
+/* -- Had to add extra lines for touch events -- */
+
+window.onmousedown = e => handleOnDown(e);
+
+window.ontouchstart = e => handleOnDown(e.touches[0]);
+
+window.onmouseup = e => handleOnUp(e);
+
+window.ontouchend = e => handleOnUp(e.touches[0]);
+
+window.onmousemove = e => handleOnMove(e);
+
+window.ontouchmove = e => handleOnMove(e.touches[0]);
+
+
+
+// //Background Set for Categories
+
+// $('.set-bg').each(function () {
+//   var bg = $(this).data('setbg');
+//   $(this).css('background-image', 'url(' + bg + ')');
+// });
+
+// //// Carousel
+
+// $(".categories__slider").owlCarousel({
+//   loop: true,
+//   margin: 0,
+//   items: 4,
+//   dots: false,
+//   nav: true,
+//   navText: ["<span class='fa fa-angle-left'><span/>", "<span class='fa fa-angle-right'><span/>"],
+//   animateOut: 'fadeOut',
+//   animateIn: 'fadeIn',
+//   smartSpeed: 1200,
+//   autoHeight: false,
+//   autoplay: true,
+//   responsive: {
+
+//       0: {
+//           items: 1,
+//       },
+
+//       480: {
+//           items: 2,
+//       },
+
+//       768: {
+//           items: 3,
+//       },
+
+//       992: {
+//           items: 4,
+//       }
+//   }
+// });
+
+
+// $('.hero__categories__all').on('click', function(){
+//   $('.hero__categories ul').slideToggle(400);
+// });
