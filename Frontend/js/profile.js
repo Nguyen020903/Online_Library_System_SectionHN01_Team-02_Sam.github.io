cf9a3924f5839038662ad5186890c656f40661bc
Hai Nguyen <s3978275@rmit.edu.vn>
2023-12-13 23:07:20 +0700

    Skeleton

---
+++ Frontend/js/profile.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Hide all tabs except the default one
+    var tabs = document.querySelectorAll('.tab-content');
+    for (var i = 1; i < tabs.length; i++) {
+        tabs[i].style.display = 'none';
+    }
+});
+
+function openTab(tabName) {
+    var tabs = document.querySelectorAll('.tab-content');
+
+    // Remove the 'active' class from all tabs
+    for (var i = 0; i < tabs.length; i++) {
+        tabs[i].classList.remove('active');
+    }
+
+    // Add the 'active' class to the selected tab
+    var currentTab = document.getElementById(tabName);
+    currentTab.classList.add('active');
+
+    // Hide all tabs except the selected one
+    for (var i = 0; i < tabs.length; i++) {
+        if (tabs[i].id === tabName) {
+            tabs[i].style.display = 'flex'; // Show the selected tab
+        } else {
+            tabs[i].style.display = 'none'; // Hide other tabs
+        }
+    }
+}
