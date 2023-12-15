a7e7a27167493c4006b226aca1dadbc75b6f7ebf
hmyyyyyy <hmyle.it@gmail.com>
2023-12-16 00:22:57 +0700

    Update addToWishlist method

--- controllers/reservationControllers.js
+++ controllers/reservationControllers.js
@@ -1,25 +1,30 @@
 const User = require('../models/user');
+const Book = require('../models/book');
 
-module.exports.add_to_cart_post = (req, res) => {
-    const book_id = req.body.book_id;
-  
-    if (!req.session.cart) {
-      req.session.cart = [];
-    }
-  
-    let count = 0;
-    for (let i = 0; i < req.session.cart.length; i++) {
-      if (req.session.cart[i].product_id === product_id) {
-        req.session.cart[i].quantity += 1;
-        count++;
+
+module.exports.add_to_wishlist_post = async (req, res) => {
+  const { bookId } = req.body;
+
+  let user = res.locals.user;
+  if (user) {
+    try {
+      const book = await Book.findById(bookId);
+
+      if (book) {
+        // Add the book_id to the user's favoriteBook array
+        user.favoriteBook.push(bookId);
+        // Save the updated user
+        await user.save();
+      } else {
+        // Handle case where book with the given ID is not found
+        return res.status(404).json({ error: 'Book not found' });
       }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'An error occurred while adding the book to the cart' });
     }
-    if (count === 0) {
-      const cart_data = {
-        book_id: book_id,
-        quantity: 1
-      };
-      req.session.cart.push(cart_data);
-    }
+  }
+
+  // Send a 200 OK response
+  res.status(200).end();
 };
-  
--- routes/reservationRoutes.js
+++ routes/reservationRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const { checkUser } = require('../middleware/authMiddleware');
+const { checkUser, requireAuth } = require('../middleware/authMiddleware');
 const router = express.Router();
 const reservationController = require('../controllers/reservationControllers');
 
-router.post('/addToCart', checkUser, reservationController.add_to_cart_post);
+router.post('/addToWishlist', requireAuth, checkUser, reservationController.add_to_wishlist_post);
 
 module.exports = router;
